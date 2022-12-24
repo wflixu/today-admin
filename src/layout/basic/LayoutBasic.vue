17e63ac048317d44b46706b03a72d5dcb0c4f0a5
lixu <wflixu@88.com>
2022-12-24 21:46:32 +0800

    test

---
+++ src/layout/basic/LayoutBasic.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">
+        <RouterView />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import { createPinia } from 'pinia'
+
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
+
 import './assets/main.css'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(createPinia());
+app.use(router);
 
 app.mount('#app')
